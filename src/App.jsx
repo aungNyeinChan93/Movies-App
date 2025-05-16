54cb6f2c7524e8f767ef188bc3c7a25e6eb05ad9
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2025-05-16 07:03:41 +0630

    optimize search by useDebounce

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import HeroSection from './components/HeroSection';
 import SearchSection from './components/SearchSection';
 import Movies from './components/Movies';
 import useGetMovies from './hooks/useGetMovies';
+import { useDebounce } from 'react-use';
 
 const App = () => {
     const [search, setSearch] = useState('');
+    const [debounceSearch, setDebounceSearch] = useState('');
+
     const API_BASE_URL = `https://api.themoviedb.org/3`;
     const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
+    useDebounce(() => setDebounceSearch(search), 1000, [search]);
+
     let end_point = `${API_BASE_URL}/movie/popular`;
-    if (search.length) {
+    if (debounceSearch.length) {
         end_point = `${API_BASE_URL}/search/movie?query=${encodeURIComponent(search)}`
     }
 
